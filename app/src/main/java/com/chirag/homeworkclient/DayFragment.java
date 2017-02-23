a2e37cbae4ca0efaf7ab286b94e2fe6a2047ab2c
spafindoople <spafindoople@gmail.com>
2017-02-23 07:08:20 -0800

    .

--- app/src/main/java/com/chirag/homeworkclient/DayFragment.java
+++ app/src/main/java/com/chirag/homeworkclient/DayFragment.java
@@ -10,11 +10,16 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 public class DayFragment extends Fragment {
     View.OnClickListener mClickListener;
     DataManager mDataManager;
+
     public DayFragment() {
         // Required
     }
@@ -40,10 +45,21 @@ public class DayFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_day, container, false);
         ListView leftColumn = (ListView) view.findViewById(R.id.left_column);
-        leftColumn.setAdapter(new AssignmentAdapter(mDataManager.getAssignments(0)));
         ListView rightColumn = (ListView) view.findViewById(R.id.right_column);
-        rightColumn.setAdapter(new AssignmentAdapter(mDataManager.getAssignments(0)));
+
+        rightColumn.setAdapter(new AssignmentAdapter(mDataManager.getAssignments(0), 1));
+        leftColumn.setAdapter(new AssignmentAdapter(mDataManager.getAssignments(0), 0));
         return view;
+    }
+
+    public List<Assignment> splitList(List<Assignment> assignments, int side) {
+        List<Assignment> ret = new ArrayList<>();
+        for (int i = 0; i < assignments.size(); i++) {
+            if (i % 2 == side) {
+                ret.add(assignments.get(i));
+            }
+        }
+        return ret;
     }
 
     @Override
@@ -59,8 +75,8 @@ public class DayFragment extends Fragment {
     private class AssignmentAdapter extends BaseAdapter {
         List<Assignment> mAssignments;
 
-        public AssignmentAdapter(List<Assignment> assignments) {
-            mAssignments = assignments;
+        public AssignmentAdapter(List<Assignment> assignments, int side) {
+            mAssignments = splitList(assignments, side);
         }
 
         public int getCount() {
@@ -84,7 +100,29 @@ public class DayFragment extends Fragment {
             ((TextView) convertView.findViewById(R.id.class_name_text)).setText(
                     mAssignments.get(position).course);
 
+            ((TextView) convertView.findViewById(R.id.assignment_name_text)).setText(
+                    mAssignments.get(position).title);
+
+            ((TextView) convertView.findViewById(R.id.assignment_day_text)).setText(
+                    dateString(mAssignments.get(position).start) +
+                    " - " +
+                    dateString(mAssignments.get(position).end));
+
+            ((TextView) convertView.findViewById(R.id.assignment_description_text)).setText(
+                    mAssignments.get(position).des);
+
             return convertView;
         }
+    }
+
+    public String dateString(Date date) {
+        String ret = "";
+        String stringDate = date.toString();
+        int year = Integer.parseInt(stringDate.substring(0,4));
+        int month = Integer.parseInt(stringDate.substring(5,7));
+        int day = Integer.parseInt(stringDate.substring(8,10));
+
+
+        return ret;
     }
 }
