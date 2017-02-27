c81b9b81567504caf38590015de6598ffd6bde2c
spafindoople <spafindoople@gmail.com>
2017-02-27 07:00:38 -0800

    improved ui

--- app/src/main/java/com/chirag/homeworkclient/Assignment.java
+++ app/src/main/java/com/chirag/homeworkclient/Assignment.java
@@ -6,16 +6,17 @@ import java.sql.Date;
  * Created by spafindoople on 2/12/17.
  */
 
+
 public class Assignment {
     public final String title;
-    public final String des;
+    public final String desc;
     public final String course;
     public final Date start;
     public final Date end;
 
-    public Assignment(String title, String des, String course, Date start, Date end) {
+    public Assignment(String title, String desc, String course, Date start, Date end) {
         this.title=title;
-        this.des = des;
+        this.desc = desc;
         this.course = course;
         this.start = start;
         this.end = end;
