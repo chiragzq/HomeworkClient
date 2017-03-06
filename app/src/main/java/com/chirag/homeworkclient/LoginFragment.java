6f5e195138b8767785458fca76d055b6646b0606
spafindoople <spafindoople@gmail.com>
2017-03-05 21:24:03 -0800

    started fancy calendar

--- app/src/main/java/com/chirag/homeworkclient/LoginFragment.java
+++ app/src/main/java/com/chirag/homeworkclient/LoginFragment.java
@@ -1,11 +1,15 @@
 package com.chirag.homeworkclient;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.TextView;
 
 
 public class LoginFragment extends Fragment {
@@ -36,6 +40,10 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         view.findViewById(R.id.login_button).setOnClickListener(mClickListener);
+
+        GridView gridView = ((GridView) view.findViewById(R.id.gridview));
+        gridView.setNumColumns(7);
+        gridView.setAdapter(new GridAdapter(getContext()));
         return view;
     }
 
@@ -63,5 +71,42 @@ public class LoginFragment extends Fragment {
 
         ((EditText)getView().findViewById(R.id.username_et)).setText("");
         ((EditText)getView().findViewById(R.id.password_et)).setText("");
+    }
+
+    public class GridAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public GridAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return 42;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        private final String[] weekday = {"S", "M", "T", "W", "T", "F", "S"};
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.calendar_cell, parent, false);
+            }
+            if(position < 7) {
+                ((TextView)convertView.findViewById(R.id.day_num)).setText(weekday[position]);
+            } else {
+                ((TextView)convertView.findViewById(R.id.day_num)).setText("hello" + position);
+            }
+            return convertView;
+        }
+
     }
 }
