a9e427aab80517cdf21d08b796002f9a301d06bb
Mauricio Narvaez <maunvz@stanford.edu>
2017-05-28 11:25:23 -0700

    Added LOGIN STUFF

--- app/src/main/java/com/chirag/homeworkclient/MainActivity.java
+++ app/src/main/java/com/chirag/homeworkclient/MainActivity.java
@@ -8,8 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-
 public class MainActivity extends FragmentActivity {
+    NetworkManager mNetworkManager;
     MainClickListener mMainClickListener;
     DataManager mDataManager;
 
@@ -44,12 +44,7 @@ public class MainActivity extends FragmentActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.activity_main, mCalendarFragment, "cal2_frag")
                 .commit();
-    }
-
-    public void tryLogin(String username, String password) {
-        findViewById(R.id.login_button).setEnabled(false);
-        Log.i("debug", "Username: " + username + " Password: " + password);
-        new LoginTask().execute(username, password);
+        mNetworkManager = new NetworkManager();
     }
 
     public void openCalender() {
@@ -65,37 +60,28 @@ public class MainActivity extends FragmentActivity {
         transaction.commit();
     }
 
-    public class LoginTask extends AsyncTask<String, Integer, Boolean> {
-        protected Boolean doInBackground(String... strings) {
-            String username = strings[0];
-            String password = strings[1];
-
-
-            if(username.equals("22pranavg")) {
-                return false;
-            }
-            return username.equals("Hi") && password.equals("Hi");
-        }
-
-        protected void onPostExecute(Boolean success) {
-            if (success) {
-                openCalender();
-            } else {
-                mLoginFragment.setLoading(false);
-                findViewById(R.id.login_failed_message).setVisibility(View.VISIBLE);
-            }
-        }
-    }
-
     public void onLoginClicked() {
         String username = ((EditText)findViewById(R.id.username_et)).getText().toString();
         String password = ((EditText)findViewById(R.id.password_et)).getText().toString();
-        tryLogin(username,password);
+        mLoginFragment.login(username, password, new LoginFragment.OnLoginListener() {
+            @Override
+            public void onLogin(String sessionId, String token) {
+                boolean success = mNetworkManager.handleLogin(sessionId, token);
+                if (success) {
+                    openCalender();
+                } else {
+                    mLoginFragment.setLoading(false);
+                    findViewById(R.id.login_failed_message).setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         mLoginFragment.setLoading(true);
     }
 
     public void onLogoutClicked() {
+        mLoginFragment.logout();
+        mNetworkManager.clearLogin();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.activity_main, mLoginFragment);
         transaction.commit();
